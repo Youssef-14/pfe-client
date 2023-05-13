753ca84a51013f55e870fbde5cd484df39490173
Youssef-14 <youssefmaatoug99@gmail.com>
2023-05-13 11:43:39 +0100

    13/05

--- back-end/src/routes/DataCenterRoutes.js
+++ back-end/src/routes/DataCenterRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const dataCenterController = require('../controllers/DataCenterController');
 const passport = require('passport');
+const { inRole , Roles } = require('../security/Rolemiddleware');
 
-router.get('/get',passport.authenticate('jwt', { session: false }) ,dataCenterController.getAllDataCenters);
+router.get('/get',passport.authenticate('jwt', { session: false }) ,inRole(Roles.Admin),dataCenterController.getAllDataCenters);
 router.get('/:id', dataCenterController.getDataCenterById);
 router.post('/add', dataCenterController.createDataCenter);
 router.put('/:id', dataCenterController.updateDataCenter);
--- back-end/src/security/Rolemiddleware.js
+++ back-end/src/security/Rolemiddleware.js
@@ -1,5 +1,17 @@
 const Roles = {
     Admin: 'Admin',
-    User: 'User'
+    User: 'Utilisateur'
 }
 
+const inRole = (...roles) => (req, res, next) => {
+    const role = roles.find(role => req.user.Role === role);
+    if (!role) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+    next();
+}
+
+module.exports = {
+    Roles,
+    inRole
+}
