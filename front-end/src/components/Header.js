99d154f465df4ba51ba6cbe68637226d97d165f9
Bouzomita Naim <damdoumbouz@gmail.com>
2023-05-14 19:29:40 +0100

    black

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -12,12 +12,12 @@ import { getUserRole } from "../_services/account.services";
 
 
 function Header() {
-    if (getUserRole()=='Admin'){
+    if (getUserRole() == 'Admin') {
         return (
             <Navbar bg="light" expand="lg">
                 <Container fluid>
                     <Link to='/Home'><img src={logo} className="logo" alt="not found" /></Link>
-    
+
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                         <Nav
@@ -31,9 +31,9 @@ function Header() {
                                 <Link to='/DC_VISUALISATION' id='h' >DCvisualization</Link></Nav.Link>
                             <Nav.Link >
                                 <Link to='/Reporting' id='h'>Reporting</Link>
-                                </Nav.Link>
-                                <Nav.Link >  
-                                    <Link to='/AccountsManagement' id='h' >Users management</Link>
+                            </Nav.Link>
+                            <Nav.Link >
+                                <Link to='/AccountsManagement' id='h' >Users management</Link>
                             </Nav.Link>
                         </Nav>
                         <Form className="d-flex">
@@ -52,45 +52,45 @@ function Header() {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>)
-    }else{
-        return(
-                <Navbar bg="light" expand="lg">
-                    <Container fluid>
-                        <Link to='/Home'><img src={logo} className="logo" alt="not found" /></Link>
-        
-                        <Navbar.Toggle aria-controls="navbarScroll" />
-                        <Navbar.Collapse id="navbarScroll">
-                            <Nav
-                                className="me-auto my-2 my-lg-0"
-                                style={{ maxHeight: '100px' }}
-                                navbarScroll
-                            >
-                                <Nav.Link >
-                                    <Link to="/Home" id='h'>Home</Link></Nav.Link>
-                                <Nav.Link >
-                                    <Link to='/DC_VISUALISATION' id='h' >DCvisualization</Link></Nav.Link>
-                                <Nav.Link >
-                                    <Link to='/Reporting' id='h'>Reporting</Link></Nav.Link>
-                            </Nav>
-                            <Form className="d-flex">
-                                <InputGroup className="mb-3">
-                                    <FormControl
-                                        type="search"
-                                        placeholder="Search"
-                                        aria-label="Search"
-                                        aria-describedby="basic-addon2"
-                                    />
-                                    <Button variant="outline-success">
-                                        <BsSearch />
-                                    </Button>
-                                </InputGroup>
-                            </Form>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
+    } else {
+        return (
+            <Navbar bg="black" expand="lg">
+                <Container fluid>
+                    <Link to='/Home'><img src={logo} className="logo" alt="not found" /></Link>
+
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Nav.Link >
+                                <Link to="/Home" id='h'>Home</Link></Nav.Link>
+                            <Nav.Link >
+                                <Link to='/DC_VISUALISATION' id='h' >DCvisualization</Link></Nav.Link>
+                            <Nav.Link >
+                                <Link to='/Reporting' id='h'>Reporting</Link></Nav.Link>
+                        </Nav>
+                        <Form className="d-flex">
+                            <InputGroup className="mb-3">
+                                <FormControl
+                                    type="search"
+                                    placeholder="Search"
+                                    aria-label="Search"
+                                    aria-describedby="basic-addon2"
+                                />
+                                <Button variant="outline-success">
+                                    <BsSearch />
+                                </Button>
+                            </InputGroup>
+                        </Form>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         );
 
-        }
+    }
 }
 
 
