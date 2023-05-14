856d2bb5fcabfec9261b7b04f663a395b2bc1a3b
Youssef-14 <youssefmaatoug99@gmail.com>
2023-05-14 10:10:04 +0100

    14/05

--- back-end/src/routes/PodRoutes.js
+++ back-end/src/routes/PodRoutes.js
@@ -7,7 +7,7 @@ router.post('/add', podController.createPod);
 router.get('/get', podController.getAllPods);
 router.get('/:id', podController.getPodById);
 router.get('/get/datacenter/:id', podController.getPodsByDatacenterId);
-router.patch('/:id', podController.updatePodById);
+router.put('/:id', podController.updatePodById);
 router.delete('/:id', podController.deletePodById);
 
 module.exports = router;
