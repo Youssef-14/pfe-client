99b110b03438cf7dc636029191b4cf8edf8a6c8a
Youssef-14 <youssefmaatoug99@gmail.com>
2023-05-14 09:58:48 +0100

    14/05

--- back-end/src/routes/DataCenterRoutes.js
+++ back-end/src/routes/DataCenterRoutes.js
@@ -4,7 +4,7 @@ const dataCenterController = require('../controllers/DataCenterController');
 const passport = require('passport');
 const { inRole , Roles } = require('../security/Rolemiddleware');
 
-router.get('/get',passport.authenticate('jwt', { session: false }) ,inRole(Roles.Admin),dataCenterController.getAllDataCenters);
+router.get('/get',/*passport.authenticate('jwt', { session: false }) ,inRole(Roles.Admin),*/dataCenterController.getAllDataCenters);
 router.get('/:id', dataCenterController.getDataCenterById);
 router.post('/add', dataCenterController.createDataCenter);
 router.put('/:id', dataCenterController.updateDataCenter);
