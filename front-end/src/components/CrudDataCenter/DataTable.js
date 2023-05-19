e9128638660354d7ba937404723ea73d4bac1aa5
Bouzomita Naim <damdoumbouz@gmail.com>
2023-05-19 02:07:49 +0100

    css preque ok

--- front-end/src/components/CrudDataCenter/DataTable.js
+++ front-end/src/components/CrudDataCenter/DataTable.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Table, Button } from 'reactstrap';
 import ModalForm from './Modal';
-import { faEdit, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import '../style/DC_VISUALISATION.css';
 import axios from 'axios';
@@ -11,7 +11,7 @@ function DataTable(props) {
   const deleteItem = (id) => {
     let confirmDelete = window.confirm('Delete item forever?');
     if (confirmDelete) {
-      axios.delete(`http://localhost:3001/datacenters/${id}`,{
+      axios.delete(`http://localhost:3001/datacenters/${id}`, {
         headers: {
           'Authorization': `Bearer ${getToken()}`
         }
