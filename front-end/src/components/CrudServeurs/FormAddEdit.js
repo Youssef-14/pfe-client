229c6ccc02d4e466915decab91060de52cbfe4a1
Bouzomita Naim <damdoumbouz@gmail.com>
2023-06-11 12:01:17 +0100

    all

--- front-end/src/components/CrudServeurs/FormAddEdit.js
+++ front-end/src/components/CrudServeurs/FormAddEdit.js
@@ -45,7 +45,7 @@ function AddEditForm(props) {
     console.log(serveur);
 
     axios.post('http://localhost:3001/serveurs/add', serveur, {
-      headers: { 
+      headers: {
         'Authorization': `Bearer ${getToken()}`
       }
     })
@@ -63,9 +63,9 @@ function AddEditForm(props) {
 
   const fetchDataCenters = async () => {
     try {
-      const response = await axios.get('http://127.0.0.1:3001/datacenters/get',{
+      const response = await axios.get('http://127.0.0.1:3001/datacenters/get', {
         headers: {
-            'Authorization': `Bearer ${getToken()}`
+          'Authorization': `Bearer ${getToken()}`
         }
       });
       setDataCenters(response.data);
@@ -99,9 +99,9 @@ function AddEditForm(props) {
     try {
       const response = await axios.get(`http://127.0.0.1:3001/pods/get/datacenter/${dataCenterId}`, {
         headers: {
-            'Authorization': `Bearer ${getToken()}`
-        } 
-    });
+          'Authorization': `Bearer ${getToken()}`
+        }
+      });
       setPods(response.data);
     } catch (error) {
       console.error(error);
@@ -110,11 +110,11 @@ function AddEditForm(props) {
 
   const fetchRacks = async (podId) => {
     try {
-      const response = await axios.get(`http://127.0.0.1:3001/racks/get/pod/${podId}`,{
+      const response = await axios.get(`http://127.0.0.1:3001/racks/get/pod/${podId}`, {
         headers: {
-            'Authorization': `Bearer ${getToken()}`
-        } 
-    });
+          'Authorization': `Bearer ${getToken()}`
+        }
+      });
       setRacks(response.data);
     } catch (error) {
       console.error(error);
@@ -140,7 +140,7 @@ function AddEditForm(props) {
       Role: form.Role
     }
     axios.put(`http://localhost:3001/serveurs/${form._id}`, serveur, {
-      headers: { 
+      headers: {
         'Authorization': `Bearer ${getToken()}`
       }
     })
@@ -160,15 +160,15 @@ function AddEditForm(props) {
   useEffect(() => {
     fetchDataCenters();
     if (props.item) {
-  const { _id, Login, Password, Model, IP, IPManagement, RAM, CPU, ConsommationRAM, ConsommationCPU, Uptime, Owner, Role, Rack } = props.item;
-  setValues({ _id, Login, Password, Model, IP, IPManagement, RAM, CPU, ConsommationRAM, ConsommationCPU, Uptime, Owner, Role, Rack });
-}
+      const { _id, Login, Password, Model, IP, IPManagement, RAM, CPU, ConsommationRAM, ConsommationCPU, Uptime, Owner, Role, Rack } = props.item;
+      setValues({ _id, Login, Password, Model, IP, IPManagement, RAM, CPU, ConsommationRAM, ConsommationCPU, Uptime, Owner, Role, Rack });
+    }
   }, [props.item]);
 
-  if(props.item){
+  if (props.item) {
     return (
-      <Form onSubmit={ submitFormEdit }>
-    
+      <Form onSubmit={submitFormEdit}>
+
         <FormGroup>
           <Label for="IPManagement">IPManagment</Label>
           <Input
@@ -180,7 +180,7 @@ function AddEditForm(props) {
             placeholder="IPManagement"
           />
         </FormGroup>
-    
+
         <FormGroup>
           <Label for="Model">Model</Label>
           <Input
@@ -305,185 +305,185 @@ function AddEditForm(props) {
         <Button>Submit</Button>
       </Form>
     );
-    }else{
-      return (
-        <Form onSubmit={submitFormAdd}>
-      
-          <FormGroup>
-            <Label for="IPManagment">IPManagment</Label>
-            <Input
-              type="text"
-              name="IPManagement"
-              id="IPManagment"
-              onChange={onChange}
-              value={form.IPManagment === null ? "" : form.IPManagement}
-              placeholder="5645154"
-            />
-          </FormGroup>
-      
-          <FormGroup>
-            <Label for="Model">Model</Label>
-            <Input
-              type="text"
-              name="Model"
-              id="Model"
-              onChange={onChange}
-              value={form.Model === null ? "" : form.Model}
-              placeholder="Model"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="IP">IP</Label>
-            <Input
-              type="text"
-              name="IP"
-              id="IP"
-              onChange={onChange}
-              value={form.IP === null ? "" : form.IP}
-              placeholder="IP"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="CPU">CPU</Label>
-            <Input
-              type="text"
-              name="CPU"
-              id="Pod"
-              onChange={onChange}
-              value={form.CPU === null ? "" : form.CPU}
-              placeholder="CPU"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="Owner">Owner</Label>
-            <Input
-              type="text"
-              name="Owner"
-              id="Owner"
-              onChange={onChange}
-              value={form.Owner === null ? "" : form.Owner}
-              placeholder="Owner"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="serveurname">serveurname</Label>
-            <Input
-              type="text"
-              name="Login"
-              id="serveurname"
-              onChange={onChange}
-              value={form.Login === null ? "" : form.Login}
-              placeholder="serveurname"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="Password">Password</Label>
-            <Input
-              type="text"
-              name="Password"
-              id="Password"
-              onChange={onChange}
-              value={form.Password === null ? "" : form.Password}
-              placeholder="password"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="Uptime">Uptime</Label>
-            <Input
-              type="text"
-              name="Uptime"
-              id="Uptime"
-              onChange={onChange}
-              value={form.Uptime === null ? "" : form.Uptime}
-              placeholder="Uptime"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="ConsommationCPU">ConsommationCPU</Label>
-            <Input
-              type="text"
-              name="ConsommationCPU"
-              id="ConsommationCPU"
-              onChange={onChange}
-              value={form.ConsommationCPU === null ? "" : form.ConsommationCPU}
-              placeholder="ConsommationCPU"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="Model">ConsommationRAM</Label>
-            <Input
-              type="text"
-              name="ConsommationRAM"
-              id="ConsommationRAM"
-              onChange={onChange}
-              value={form.ConsommationRAM === null ? "" : form.ConsommationRAM}
-              placeholder="ConsommationRAM"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="Role">Role</Label>
-            <Input
-              type="text"
-              name="Role"
-              id="Role"
-              onChange={onChange}
-              value={form.Role === null ? "" : form.Role}
-              placeholder="Role"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="DataCenter">DataCenter</Label>
-            <br/>
-            <select value={selectedDataCenter} onChange={handleDataCenterChange} required>
-              <option value="">Sélectionner un Data Center</option>
-              {dataCenters.map((dataCenter) => (
-                <option key={dataCenter._id} value={dataCenter._id}>
-                  {dataCenter.Libelle}
-                </option>
-              ))}
-            </select>
-          </FormGroup>
-          <FormGroup>
-            <Label for="Pod">Pod</Label>
-              <br/>
-              <select value={selectedPod} onChange={handlePodChange} required>
-              <option value="">Sélectionner un Pod</option>
-              {pods.map((pod) => (
-                <option key={pod._id} value={pod._id}>
-                  {pod.Libelle}
-                </option>
-              ))}
-            </select>
-          </FormGroup>
-          <FormGroup>
-            <Label for="Rack">Rack</Label>
-            <br/>
-              <select name="Rack" id="Rack" onChange={onChange} required>
-              <option value="">Sélectionner un Rack</option>
-              {racks.map((rack) => (
-                <option key={rack._id} value={rack._id}>
-                  {rack.Nom}
-                </option>
-              ))}
-            </select>
-          </FormGroup>
-          <FormGroup>
-            <Label for="RAM">RAM</Label>
-            <Input
-              type="text"
-              name="RAM"
-              id="RAM"
-              onChange={onChange}
-              value={form.RAM === null ? "" : form.RAM}
-              placeholder="RAM"
-            />
-          </FormGroup>
-          <Button>Submit</Button>
-        </Form>
-      );
-      }
-    }
-  
+  } else {
+    return (
+      <Form onSubmit={submitFormAdd}>
+
+        <FormGroup>
+          <Label for="IPManagment">IPManagment</Label>
+          <Input
+            type="text"
+            name="IPManagement"
+            id="IPManagment"
+            onChange={onChange}
+            value={form.IPManagment === null ? "" : form.IPManagement}
+            placeholder="5645154"
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="Model">Model</Label>
+          <Input
+            type="text"
+            name="Model"
+            id="Model"
+            onChange={onChange}
+            value={form.Model === null ? "" : form.Model}
+            placeholder="Model"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="IP">IP</Label>
+          <Input
+            type="text"
+            name="IP"
+            id="IP"
+            onChange={onChange}
+            value={form.IP === null ? "" : form.IP}
+            placeholder="IP"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="CPU">CPU</Label>
+          <Input
+            type="text"
+            name="CPU"
+            id="Pod"
+            onChange={onChange}
+            value={form.CPU === null ? "" : form.CPU}
+            placeholder="CPU"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="Owner">Owner</Label>
+          <Input
+            type="text"
+            name="Owner"
+            id="Owner"
+            onChange={onChange}
+            value={form.Owner === null ? "" : form.Owner}
+            placeholder="Owner"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="serveurname">serveurname</Label>
+          <Input
+            type="text"
+            name="Login"
+            id="serveurname"
+            onChange={onChange}
+            value={form.Login === null ? "" : form.Login}
+            placeholder="serveurname"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="Password">Password</Label>
+          <Input
+            type="text"
+            name="Password"
+            id="Password"
+            onChange={onChange}
+            value={form.Password === null ? "" : form.Password}
+            placeholder="password"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="Uptime">Uptime</Label>
+          <Input
+            type="text"
+            name="Uptime"
+            id="Uptime"
+            onChange={onChange}
+            value={form.Uptime === null ? "" : form.Uptime}
+            placeholder="Uptime"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="ConsommationCPU">ConsommationCPU</Label>
+          <Input
+            type="text"
+            name="ConsommationCPU"
+            id="ConsommationCPU"
+            onChange={onChange}
+            value={form.ConsommationCPU === null ? "" : form.ConsommationCPU}
+            placeholder="ConsommationCPU"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="Model">ConsommationRAM</Label>
+          <Input
+            type="text"
+            name="ConsommationRAM"
+            id="ConsommationRAM"
+            onChange={onChange}
+            value={form.ConsommationRAM === null ? "" : form.ConsommationRAM}
+            placeholder="ConsommationRAM"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="Role">Role</Label>
+          <Input
+            type="text"
+            name="Role"
+            id="Role"
+            onChange={onChange}
+            value={form.Role === null ? "" : form.Role}
+            placeholder="Role"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="DataCenter">DataCenter</Label>
+          <br />
+          <select value={selectedDataCenter} onChange={handleDataCenterChange} required>
+            <option value="">Sélectionner un Data Center</option>
+            {dataCenters.map((dataCenter) => (
+              <option key={dataCenter._id} value={dataCenter._id}>
+                {dataCenter.Libelle}
+              </option>
+            ))}
+          </select>
+        </FormGroup>
+        <FormGroup>
+          <Label for="Pod">Pod</Label>
+          <br />
+          <select value={selectedPod} onChange={handlePodChange} required>
+            <option value="">Sélectionner un Pod</option>
+            {pods.map((pod) => (
+              <option key={pod._id} value={pod._id}>
+                {pod.Libelle}
+              </option>
+            ))}
+          </select>
+        </FormGroup>
+        <FormGroup>
+          <Label for="Rack">Rack</Label>
+          <br />
+          <select name="Rack" id="Rack" onChange={onChange} required>
+            <option value="">Sélectionner un Rack</option>
+            {racks.map((rack) => (
+              <option key={rack._id} value={rack._id}>
+                {rack.Nom}
+              </option>
+            ))}
+          </select>
+        </FormGroup>
+        <FormGroup>
+          <Label for="RAM">RAM</Label>
+          <Input
+            type="text"
+            name="RAM"
+            id="RAM"
+            onChange={onChange}
+            value={form.RAM === null ? "" : form.RAM}
+            placeholder="RAM"
+          />
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+    );
+  }
+}
+
 
 
 export default AddEditForm;
